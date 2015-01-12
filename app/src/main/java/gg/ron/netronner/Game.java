8d6f92847c1e9812a9f2ef1f13d336d5798c8d7b
Caligin@Theleth <caligin35@yahoo.it>
2015-01-12 02:23:21 +0100

    enh: sketching game list items layout

--- app/src/main/java/gg/ron/netronner/Game.java
+++ app/src/main/java/gg/ron/netronner/Game.java
@@ -8,11 +8,24 @@ public class Game {
         HANCHAN, TONPUUSEN;
     }
 
+    public Type gameType;
+    public Date started;
     public String player1;
     public String player2;
     public String player3;
     public String player4;
-    public Type gameType;
-    public Date started;
+
+    public static Game of(Type gameType, Date started, String player1, String player2, String player3, String player4) {
+        final Game g = new Game();
+        g.gameType = gameType;
+        g.started = started;
+        g.player1 = player1;
+        g.player2 = player2;
+        g.player3 = player3;
+        g.player4 = player4;
+        return g;
+    }
+
+
 
 }
--- app/src/main/java/gg/ron/netronner/GamesActivity.java
+++ app/src/main/java/gg/ron/netronner/GamesActivity.java
@@ -3,12 +3,21 @@ package gg.ron.netronner;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class GamesActivity extends ActionBarActivity {
@@ -18,7 +27,11 @@ public class GamesActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
         final ListView gamesListView = (ListView) findViewById(R.id.games_list);
-//        gamesListView.setAdapter(new ArrayAdapter<>());
+        final GamesArrayAdapter adapter = new GamesArrayAdapter(this);
+        gamesListView.setAdapter(adapter);
+        adapter.add(Game.of(Game.Type.HANCHAN, new Date(), "foo", "bar", "sig", "mik"));
+        adapter.add(Game.of(Game.Type.HANCHAN, new Date(), "ang", "bid", "pel", "ala"));
+        adapter.add(Game.of(Game.Type.TONPUUSEN, new Date(), "inu", "emm", "mai", "dan"));
     }
 
 
@@ -44,16 +57,49 @@ public class GamesActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class GamesArrayAdapter extends ArrayAdapter<Game> {
+    public static class GamesArrayAdapter extends BaseAdapter {
 
+private final Context context;
+        private final List<Game> games;
+        public GamesArrayAdapter(Context context) {
+            this.context = context;
+            this.games = new ArrayList<>();
+        }
 
-        public GamesArrayAdapter(Context context, int resource) {
-            super(context, resource);
+        @Override
+        public int getCount() {
+            return games.size();
+        }
+
+        @Override
+        public Game getItem(int position) {
+            return games.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public void add(Game game){
+            this.games.add(game);
+            notifyDataSetChanged();
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return super.getView(position, convertView, parent);
+            final Game item = getItem(position);
+            final View itemView = (RelativeLayout) LayoutInflater.from(context).inflate(R.layout.game_list_item, parent, false);
+            ((ImageView)itemView.findViewById(R.id.south_round_icon)).setVisibility(item.gameType == Game.Type.HANCHAN ? View.VISIBLE : View.INVISIBLE);
+            ((TextView)itemView.findViewById(R.id.game_type_label)).setText(item.gameType == Game.Type.HANCHAN ? "Hanchan" : "Tonpuusen");
+            ((TextView)itemView.findViewById(R.id.game_date_label)).setText(item.started.toString());
+            ((TextView)itemView.findViewById(R.id.player_1_name)).setText(item.player1);
+            ((TextView)itemView.findViewById(R.id.player_2_name)).setText(item.player2);
+            ((TextView)itemView.findViewById(R.id.player_3_name)).setText(item.player3);
+            ((TextView)itemView.findViewById(R.id.player_4_name)).setText(item.player4);
+            return itemView;
         }
+
+
     }
 }
