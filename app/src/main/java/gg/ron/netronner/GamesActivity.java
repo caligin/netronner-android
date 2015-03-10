dc1a0b6b127720b4ecca93d3904fecf766dc64cc
Caligin@Theleth <caligin35@yahoo.it>
2015-03-10 01:12:19 +0100

    enh: format timestamps as datetimes

--- app/src/main/java/gg/ron/netronner/GamesActivity.java
+++ app/src/main/java/gg/ron/netronner/GamesActivity.java
@@ -9,8 +9,13 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 
 public class GamesActivity
@@ -25,17 +30,11 @@ public class GamesActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
         final ListView gamesListView = (ListView) findViewById(R.id.games_list);
-        adapter = new SimpleCursorAdapter(
-                this,
-                R.layout.game_list_item,
-                null,
-                new String[]{GamesRepository.Fields.timestamp, GamesRepository.Fields.east, GamesRepository.Fields.south, GamesRepository.Fields.west, GamesRepository.Fields.north},
-                new int[]{R.id.game_date_label, R.id.player_1_name, R.id.player_2_name, R.id.player_3_name, R.id.player_4_name},
-                0);
-        //todo viewbinder to convert timestamp to readable date. find a way to delegate everything else to default VB
+        adapter = makeAdapter();
         gamesListView.setAdapter(adapter);
         getLoaderManager().initLoader(GAMES_LOADER_ID, null, this);
     }
+
 
     @Override
     protected void onStart() {
@@ -90,4 +89,27 @@ public class GamesActivity
     public void onLoaderReset(Loader<Cursor> loader) {
         // do nothing?
     }
+
+    private SimpleCursorAdapter makeAdapter() {
+        final SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                R.layout.game_list_item,
+                null,
+                new String[]{GamesRepository.Fields.timestamp, GamesRepository.Fields.east, GamesRepository.Fields.south, GamesRepository.Fields.west, GamesRepository.Fields.north},
+                new int[]{R.id.game_date_label, R.id.player_1_name, R.id.player_2_name, R.id.player_3_name, R.id.player_4_name},
+                0);
+        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (view.getId() == R.id.game_date_label) {
+                    final Date date = new Date(Long.parseLong(cursor.getString(columnIndex)));
+                    ((TextView) view).setText(DateFormat.getDateTimeInstance().format(date));
+                    return true;
+                }
+                return false;
+            }
+        });
+        return adapter;
+    }
+
 }
